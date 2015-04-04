d568ddc0b62186a1e97ce6cd9b94a486c70432d8
RespeckKnuckles <RespeckKnuckles1@gmail.com>
2015-04-04 13:56:22 -0400

    updating 4.4.2015 build

---
+++ compiled/compress.sh
@@ -0,0 +1,14 @@
+echo "removing old versions"
+rm *.zip
+echo "compressing linux version"
+d=$(date +'%m.%d.%Y')
+zip -r pagi_linux_newest.zip pagi_linux.x86 pagi_linux.x86_64 pagi_linux_Data 
+cp pagi_linux_newest.zip "pagi_linux_${d}.zip"
+
+echo "compressing mac version"
+zip -r pagi_mac_newest.zip pagi_mac.*
+cp pagi_mac_newest.zip "pagi_mac_${d}.zip"
+
+echo "compressing windows version"
+zip -r pagi_windows_newest.zip pagi_windows.exe pagi_windows_Data
+cp pagi_windows_newest.zip "pagi_windows_${d}.zip"
