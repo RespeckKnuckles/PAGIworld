be26dfa48f699b3edf2c0d6eafd3a2014686b773
RespeckKnuckles <RespeckKnuckles1@gmail.com>
2015-06-17 14:20:16 -0400

    Uploading version 0.2.2

--- source/Assets/GlobalVariables.cs
+++ source/Assets/GlobalVariables.cs
@@ -7,7 +7,7 @@ using System.Text.RegularExpressions;
 public class GlobalVariables : MonoBehaviour {
 
 	public static bool androidBuild = true;
-	public static string versionNumber = "0.2.1";
+	public static string versionNumber = "0.2.2";
 	
 	public static int portNumber = 42209;
 
